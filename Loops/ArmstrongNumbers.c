648a7b05822bb646859832aea04c3323521d2368
Sanskriti Rana <sanskritikrana@gmail.com>
2026-09-10 19:20:56 +0530

    Armstrong Numbers

---
+++ Loops/ArmstrongNumbers.c
@@ -0,0 +1,38 @@
+/*
+You are given a 3-digit number n, Find whether it is an Armstrong number or not.
+
+An Armstrong number of three digits is a number such that the sum of the cubes of its digits is equal to the number itself. 371 is an Armstrong number since 33 + 73 + 13 = 371. 
+
+Examples:
+
+Input: n = 153
+Output: true
+Explanation: 153 is an Armstrong number since 13 + 53 + 33 = 153. 
+Input: n = 372
+Output: false
+Explanation: 372 is not an Armstrong number since 33 + 73 + 23 = 378. 
+Input: n = 100
+Output: false
+Explanation: 100 is not an Armstrong number since 13 + 03 + 03 = 1. 
+Constraints:
+100 ≤ n <1000 
+*/
+
+class Solution {
+  public:
+    bool armstrongNumber(int n) {
+        
+        if (n < 0) return false;
+
+        int sum = 0;
+
+        for (int i = n; i != 0; i = i / 10)
+        {
+            int digit = i % 10;
+            sum = sum + digit*digit*digit ;
+        }
+
+        return sum == n;
+
+    }
+};
