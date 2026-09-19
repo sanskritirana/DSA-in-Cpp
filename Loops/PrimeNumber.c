2dc440d052498222b77fc6779a2c5d0c54ab74f6
Sanskriti Rana <sanskritikrana@gmail.com>
2026-09-19 23:48:17 +0530

    Prime Number

---
+++ Loops/PrimeNumber.c
@@ -0,0 +1,34 @@
+/*
+Given a number n, determine whether it is a prime number or not.
+Note: A prime number is a number greater than 1 that has no positive divisors other than 1 and itself.
+
+Examples :
+
+Input: n = 7
+Output: true
+Explanation: 7 has exactly two divisors: 1 and 7, making it a prime number.
+Input: n = 25
+Output: false
+Explanation: 25 has more than two divisors: 1, 5, and 25, so it is not a prime number.
+Input: n = 1
+Output: false
+Explanation: 1 has only one divisor (1 itself), which is not sufficient for it to be considered prime.
+Constraints:
+
+1 ≤ n ≤ 109
+
+*/
+
+class Solution {
+  public:
+    bool isPrime(int n) {
+        if (n <= 1) return false;
+
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0)
+                return false;
+        }
+
+        return true;
+    }
+};
